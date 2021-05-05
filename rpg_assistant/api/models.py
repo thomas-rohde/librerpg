7ac37d6dd9f22e665cac0b752b6c56d065a28f5c
thomas-rohde <thomasrohde45@gmail.com>
2021-05-04 23:06:34 -0300

    ep3

--- rpg_assistant/api/models.py
+++ rpg_assistant/api/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+import string
+import random
+
+def generate_unique_code():
+    lenght = 10
+
+    while True:
+        code = ''.join(random.choices(string.ascii_uppercase, k=lenght))
+        if Room.objects.filter(code=code).count() == 0:
+            break
+    
+    return code
 
 # Create your models here.
+
+class Room(models.Model):
+    code = models.CharField(max_lenght=8, default="", unique=True) #making the room unique, 
+    host = models.CharField(max_length=50, unique=True)
+    guest_can_pause = models.BooleanField(null=False, default=False)
+    votes_to_skip = models.IntegerField(null=False, default=1) #votes to skip the music
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    #def is_host_this():
--- rpg_assistant/api/views.py
+++ rpg_assistant/api/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from rest_framework import generics
+from .serializer import RoomSerializer
+from .models import Room
 
 # Create your views here.
-def main(request):
-    return HttpResponse("Hello")
+
+class RoomView(generics.ListAPIView): #return all the room objects to a way to view
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer
